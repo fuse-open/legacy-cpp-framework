c0c2281e95ddaeb5882d7ce75ed21ca0027f4655
Morten Daniel Fornes <mdfornes@gmail.com>
2018-05-12 05:09:59 +0200

    upgrade to VS2017
    This upgrades to VS2017 C++ compilers.

--- build.bat
+++ build.bat
@@ -1,26 +1,13 @@
 @echo off
 pushd "%~dp0"
-set GENERATOR=Visual Studio 12
-
-rem Load MSVC12
-if not defined VSINSTALLDIR (
-    call "%VS120COMNTOOLS%\vsvars32.bat"
-)
-
-rem Generate and build (x86)
-mkdir build\x86
-pushd build\x86
-cmake -G"%GENERATOR%" "%~dp0" || goto ERROR
-msbuild uno-base.sln /p:Configuration=Debug /m || goto ERROR
-msbuild uno-base.sln /p:Configuration=Release /m || goto ERROR
-popd
+set GENERATOR=Visual Studio 15 2017
 
 rem Generate and build (x64)
 mkdir build\x64
 pushd build\x64
 cmake -G"%GENERATOR% Win64" "%~dp0" || goto ERROR
-msbuild uno-base.sln /p:Configuration=Debug /m || goto ERROR
-msbuild uno-base.sln /p:Configuration=Release /m || goto ERROR
+cmake --build . --use-stderr -- /p:Configuration=Debug /m || goto ERROR
+cmake --build . --use-stderr -- /p:Configuration=Release /m || goto ERROR
 popd
 
 rem Done
